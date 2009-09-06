98ade46a6a5f90a845a0199b6665583cc0bb428a
Hibri Marzook <hibri@hibri.net>
2009-09-06 09:42:16 +0100

    added

---
+++ cleancode/CleanCode/CleanCode.Sudoku.Tests/AcceptanceTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace CleanCode.Sudoku.Tests
+{
+    [TestFixture]
+    public class AcceptanceTest
+    {  
+        [Test]
+	public void easyProblem() {
+		check(SudokuExamples.EASY_PROBLEM, SudokuExamples.EASY_SOLUTION);
+	}
+
+	[Test]
+	public void hardProblem() {
+		check(SudokuExamples.HARD_PROBLEM, SudokuExamples.HARD_SOLUTION);
+	}
+
+    [Test]
+	public void impossible() {
+		int[,] problemGrid = Sudoku.parseProblem(SudokuExamples.NOT_SOLVABLE_PROBLEM);
+		int[,] actualSolution = Sudoku.solve(problemGrid);
+		Assert.IsNull(actualSolution);
+	}
+
+	private void check(String problem, String solution) {
+		int[,] solutionGrid = Sudoku.parseProblem(solution);
+		int[,] problemGrid = Sudoku.parseProblem(problem);
+		int[,] actualSolution = Sudoku.solve(problemGrid);
+		Assert.That(Sudoku.prettyPrint(actualSolution) , Is.EqualTo( Sudoku.prettyPrint(solutionGrid) ));
+	}
+    }
+}
